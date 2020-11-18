117c3cb6bd452ea9edd0a33692a17a7e2f31f5f9
lanwar-prog <lanwar@mail.ru>
2020-11-18 10:58:53 +0400

    Commit

--- 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task18.task1813;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
 
 /* 
 AmigoOutputStream
@@ -20,11 +19,54 @@ AmigoOutputStream
 5. Метод close() должен сначала вызвать метод flush(), затем дописать текст, затем закрыть поток.
 */
 
-public class AmigoOutputStream {
-    public static String fileName = "C:/tmp/result.txt";
+public class AmigoOutputStream extends FileOutputStream {
+    public static String fileName = "C:/intel/result.txt";
+    private FileOutputStream component;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        new AmigoOutputStream(new FileOutputStream(fileName));
+
+    public AmigoOutputStream(FileOutputStream component) throws FileNotFoundException {
+        super("null");
+        this.component =component;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        component.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        component.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        component.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        component.flush();
+        String date = "JavaRush © All rights reserved.";
+        write(date.getBytes());
+        component.close();
+    }
+
+    @Override
+    public FileChannel getChannel() {
+        return component.getChannel();
+    }
+
+
+    @Override
+    public void flush() throws IOException {
+
+
+        component.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new AmigoOutputStream(new FileOutputStream(fileName)).close();
     }
 
 }
